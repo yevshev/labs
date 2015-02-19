bec067d873da40ff3e601d377f27e67a46697a09
yevshev <yshevekhman@gmail.com>
2015-02-19 14:56:15 -0500

    sync classwork

---
+++ ch08_09.c
@@ -0,0 +1,39 @@
+/*Name: ch08_09.c
+ *Purpose: generates "random walk"
+ *Date: 02/19/2015
+ *Author: Yevgeniy Shevekhman
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char board[10][10];
+  int i, j;
+	char k;
+	srand();
+
+	for (i = 0; i < 10; i++){
+		for(j = 0; j < 10; j++){
+			board[i][j] = '.';
+		}
+	}
+
+	board[0][0] = 'A'
+	
+
+	for (k = 'A'; k < 'Z'; k++) {
+		switch (r) {
+			case 0:
+				board[][] = alpha;
+				break;
+
+	for(i = 0; i < 10; i++){
+		for(j = 0; j < 10; j++)
+			
+	}
+
+
+ 	return 0;
+}
